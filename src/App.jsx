d4eb7daba05c4b013044c548066b60301587f6e0
dannyarena <arenadanny71@gmail.com>
2025-05-27 00:47:44 +0200

    add function and button to modify and save articles

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
   const [articoli, setArticoli] = useState(["Il mio primo articolo","Viaggio a Roma", "Cosa ho mangiato oggi"]);
   const [nuovoTitolo, setNuovoTitolo] = useState(""); //stato del form
 
+  const [indexInModifica, setIndexInModifica] = useState(null);  //stato che memmorizza l'indice da modficare
+  const [titoloModificato, setTitoloModificato] = useState ("");  //stato che memorizza cosa stiamo scrivendo
+
   function handleSubmit(e) {
     e.preventDefault(); // impedisce il refresh
     setArticoli([...articoli, nuovoTitolo]);  // aggiunge il nuovo articolo
@@ -22,14 +25,32 @@ function App() {
     <div>
       <h1>Lista articoli</h1>
 
-        {/* elenco che con il metodo .map mostra a schermo il contenuto dell'array */}
+        {/* lista che con il metodo .map mostra a schermo il contenuto dell'array */}
       <ul>
         {articoli.map((titolo, index) => (
-          <li key={index}>{titolo}
+          <li key={index}>
+            {indexInModifica === index ? (
+               <>
+                <input
+                type="text"
+                value={titoloModificato}
+                onChange={(e) => setTitoloModificato(e.target.value)}
+              />
+              <button onClick={salvaModifica}>Salva</button>
+               </>
+                ) : (
+              <span>{titolo}</span>
+            )}
+
+          <button onClick={() => {
+            setIndexInModifica(index);
+            setTitoloModificato(titolo);
+          }}>✏️</button>
           <button onClick={() => rimuoviArticolo(index)}>🗑</button>
           </li>
         ))}
       </ul>
+      
 
       <form onSubmit={handleSubmit}>
         <input
@@ -41,6 +62,13 @@ function App() {
       </form>
     </div>
   )
+  function salvaModifica() {
+    const copia = [...articoli];  //viene fatta una copia dell'array precedente
+    copia[indexInModifica] = titoloModificato; // Sostituisce l'articolo all'indice scelto con il nuovo titolo scritto dall'utente
+    setArticoli(copia);// Aggiorna lo stato articoli con la nuova lista modificata
+    setIndexInModifica(null);  // si esce fuori dalla modalità modifica
+  } 
+  
 }
 
 export default App;

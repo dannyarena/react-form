a725256d1b9ea201bef082ad7c616076319ac2c2
dannyarena <arenadanny71@gmail.com>
2025-05-26 15:53:05 +0200

    create array of articles and its state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
 function App() {
+
+  // "articoli" contiene l'array, setArticoli può modificare quell'array
+  const [articoli, setArticoli] = useState(["Il mio primo articolo","Viaggio a Roma", "Cosa ho mangiato oggi"]);
+
   return (
     <div>
       <h1>Lista articoli</h1>
